26e67cc6ff70d00a404e6d61cab4bedc57ddfc8d
Stefan `Sec` Zehl <sec@42.org>
2001-01-27 18:29:38 +0000

    removed a -Wall

--- ps.c
+++ ps.c
@@ -1,10 +1,8 @@
-#ifndef lint
+#if 0
 static char copyright[] =
 "@(#) Copyright (c) 1990, 1993, 1994\n\
 	The Regents of the University of California.  All rights reserved.\n";
-#endif /* not lint */
 
-#ifndef lint
 static char sccsid[] = "@(#)ps.c	8.4 (Berkeley) 4/2/94";
 #endif /* not lint */
 
